653528a559f48922f0f220d8df073badf4bccf0b
RoshiniRaj <83070973+RoshiniRaj@users.noreply.github.com>
2021-05-02 14:14:41 +0530

    Week3.Assignments

---
+++ assignments/student/Student.java
@@ -0,0 +1,29 @@
+package org.student;
+
+import org.department.Department;
+
+public class Student extends Department {
+	public void studentName() {
+		
+	}
+	public void studentDept() {
+			
+    }
+    public void studentId() {
+			
+    }
+    public static void main(String[] args) {
+	
+	
+	Student std = new Student();
+	std.collegeCode();
+	std.collegeName();
+	std.collegeRank();
+	std.deptName();
+	std.studentDept();
+	std.studentId();
+	std.studentName();
+
+	}
+
+}
---
+++ assignments/system/Desktop.java
@@ -0,0 +1,18 @@
+package org.system;
+
+public class Desktop extends Computer {
+
+	public static void main(String[] args) {
+		
+		Desktop obj = new Desktop();
+		obj.computerModel();
+		obj.desktopSize();
+		
+
+	}
+
+	public void desktopSize() {
+		
+	}
+
+}
